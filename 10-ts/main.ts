d40b5de0b3a5260aa0044be98a6d88799f18ef17
Tim Kruse <coding@mrtimeey.com>
2024-12-09 21:58:53 +0100

    prep day 10

---
+++ 10-ts/main.ts
@@ -0,0 +1,13 @@
+import { readFileSync } from 'fs'
+import {pt1, pt2} from "./src/abService";
+
+const readFileLines = (filename: string): string[] =>
+    readFileSync(filename).toString('utf-8').trimEnd().split('\n').map((line: string) => line.trimEnd())
+
+const inputLines = readFileLines('./src/input.txt')
+
+console.log('10.12.2023')
+const startP1 = Date.now()
+console.log(`Part 1: ${pt1(inputLines)} - Duration: ${Date.now() - startP1}`)
+const startP2 = Date.now()
+console.log(`Part 2: ${pt2(inputLines)} - Duration: ${Date.now() - startP2}`)
