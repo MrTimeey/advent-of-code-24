ce0a0ac031c679ada923fb916bc6d68a4686d0fd
Tim Kruse <coding@tim-kruse.com>
2024-12-20 13:28:21 +0100

    prep day 16

---
+++ 16-ts/src/mazeService.test.ts
@@ -0,0 +1,19 @@
+import { expect, test } from 'vitest'
+import { pt1, pt2 } from './mazeService'
+
+test('Test input part 1', () => {
+  const input = [
+    '',
+  ]
+
+  expect(pt1(input)).toBe(0)
+})
+
+test('Test input part 2', () => {
+  const input = [
+    '',
+  ]
+
+  expect(pt2(input)).toBe(0)
+})
+
